8e090b9a047ecc66c61a25d150424fa543fc6df5
Edgar HIPP <hipp.edg@gmail.com>
2017-01-14 17:28:14 +0100

    Add comments

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import io from 'socket.io-client'
 import './App.css';
 
+// NOTATION: Tu n'as pas encore beaucoup avancé, c'est dommage.
+
+// NOTATION: Utilise plutot const ici
 let socket = io(`http://localhost:2479`);
 
 class Steak extends React.Component {
@@ -16,10 +19,15 @@ class Steak extends React.Component {
 			]
 		};
   }
-	
+
   render() {
+	  // NOTATION: Utilise plutot const ici
 		var boutdesteak = []
-		
+
+		// NOTATION: Pour info, à la place du for, tu peux écrire :
+		// const boutdesteak = this.state.meat.map(function (meat) {
+		//     return <div className="meat" style={{top: 160+meat[i].y*16, left: 160+meat[i].x*16 }}></div>;
+		// })
 		for(let i=0; i<this.state.meat.length; i++){
 			boutdesteak.push(<div className="meat" style={{top: 160+this.state.meat[i].y*16, left: 160+this.state.meat[i].x*16 }}></div>);
 		}
